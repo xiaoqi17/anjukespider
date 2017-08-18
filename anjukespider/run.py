a213b88d4fa897dbd28134d869d41a1c090f0815
xiaoqi17 <ftkhg@163.com>
2017-08-18 16:33:30 +0800

    Update run.py

--- anjukespider/run.py
+++ anjukespider/run.py
@@ -3,7 +3,7 @@ from multiprocessing import Pool
 import zufang
 import xinfan
 import ershoufang
-
+import time
 
 def xinfan_run(page):
     urls = xinfan.page_html(page)
@@ -29,8 +29,11 @@ def zufang_run(page):
 if __name__ == '__main__':
 
     pool = Pool()
+    groups = ()
     pool.map(xinfan_run,[x for x in range(1,61)] )
+    time.sleep(600)  #暂停10分钟
     pool.map(ershoufang_run,[x for x in range(1,118)])
+    time.sleep(600)  #暂停10分钟
     pool.map(zufang_run,[x for x in range(1,51)])
     pool.close()
     pool.join()
